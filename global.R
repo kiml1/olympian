a6777143aac6f5b908dad54f99eaa2510f445e5f
Lucas <kimlucas@gmail.com>
2020-06-05 03:17:12 -0300

    added global.R file to load libraries and datasets

---
+++ global.R
@@ -0,0 +1,7 @@
+#### libraries ####
+library(shiny)
+library(shinythemes)
+
+#### data set ####
+athlete_events.df <- read.csv("./dataset/athlete_events.csv")
+noc_regions.df <- read.csv("./dataset/noc_regions.csv")
